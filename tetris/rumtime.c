21bb5fc186496cea0e3e6aeb7ce16931863de399
konodiodaa <44199257+konodiodaa@users.noreply.github.com>
2019-04-21 09:35:10 +0800

    better version

--- tetris/rumtime.c
+++ tetris/rumtime.c
@@ -5,6 +5,7 @@
 #include "eventhandle.h"
 #include "font.h"
 #include <stdio.h>
+#include <string.h>
 int piece[7][2][4] = {
 	{{0,1,1,0},{0,1,1,0}},
 	{{1,1,1,1},{0,0,0,0}},
@@ -28,19 +29,19 @@ int piece_color[7][3] = {
 void rungame(SDL_Window* gwindow,SDL_Renderer* grender,int bsize,int bnumberx,int bnumbery)
 {
 
-   	int end = 0, redraw = 1;
+   	int end = 0;
+    int redraw = 1, put = 0;
 	int key;
 	int speed = 1, line = 0;
-	int pause = 0, put = 0;
-	int game = 0;
+	int pause = 0;
 	int startx =(bnumberx-4)/2;
 	int bricktype, positionx, positiony, rot_case;
-	int xshift, yshift, clearlinenum;
-	char title[100];
+	int xshift, yshift;
+	int clearlinenum;
 	char currentSpeed[100];
 	char score[100];
-	snprintf(title, 100, "lines=%d,speed=%d", line, speed);
-	snprintf(currentSpeed,100,"current speed is :%d",speed);
+	snprintf(currentSpeed,100,"Current speed: %d",speed);
+    snprintf(score, 255, "Score: %d", line);
     SDL_Texture* pauseText=NULL;
     SDL_Texture* speedupText=NULL;
     SDL_Texture* speedownText=NULL;
@@ -64,10 +65,12 @@ void rungame(SDL_Window* gwindow,SDL_Renderer* grender,int bsize,int bnumberx,in
 
 	int t = SDL_GetTicks();
 	while (!end) {
-		if (SDL_GetTicks() - t > 3*(101-speed) && !pause) {
+		if (SDL_GetTicks() - t > 3*(101-speed) && !pause)
+        {
 			if (rotatedValid(board,piece,bricktype, positionx, positiony + 1, rot_case,bnumberx,bnumbery))
 				positiony++;
-			else put = 1;
+			else
+                put = 1;
 
 			t = SDL_GetTicks();
 			redraw = 1;
@@ -79,8 +82,7 @@ void rungame(SDL_Window* gwindow,SDL_Renderer* grender,int bsize,int bnumberx,in
 				clearlinenum = placeBrick(board,piece,bricktype, positionx, positiony, rot_case,bnumberx,bnumbery);
 				if (clearlinenum) {
 					line += clearlinenum;
-					snprintf(title, 255, "lines=%d,speed=%d", line, speed);
-					SDL_SetWindowTitle(gwindow, title);
+                    snprintf(score,100, "Score: %d", line);
 				}
 			}
 
@@ -95,40 +97,46 @@ void rungame(SDL_Window* gwindow,SDL_Renderer* grender,int bsize,int bnumberx,in
 			//check if lost
             if (!rotatedValid(board,piece,bricktype, positionx, positiony + 1, rot_case,bnumberx,bnumbery))
             {
-				printf("game %d: %d lines\n", ++game, line);
 				memset(board, 0, bnumberx*bnumbery*sizeof(int));
 				line = 0;
-				snprintf(title, 255, "lines=%d,speed=%d", line, speed);
-				SDL_SetWindowTitle(gwindow, title);
+				snprintf(score,100, "Score: %d", line);
 			}
 		}
 
-		if (redraw) {
+		if (redraw)
+        {
 
 			SDL_SetRenderDrawColor(grender, 0, 0, 0, 0);
 			SDL_RenderClear(grender);
+            showfont(scoreText,grender,530,100,100,25,score);
 			showfont(currentSpeedText,grender,530,200,200,25,currentSpeed);
             showfont( pauseText,grender, 530,500,250,25,"Press: P to stop game");
             showfont(speedupText,grender,610,550,150,25,"+ to speed up");
             showfont(speedownText,grender,615,600,170,25,"- to speed down");
+
 			box.w = bsize - 1;
 			box.h = bsize - 1;
 
 			SDL_SetRenderDrawColor(grender, 50, 50, 50, 0);
 
-			for (int y=0; y<bnumbery; y++) {
-				for (int x=0; x<bnumberx; x++) {
+			for (int y=0; y<bnumbery; y++)
+            {
+				for (int x=0; x<bnumberx; x++)
+				{
 					box.x = x*bsize;
                     box.y = y*bsize;
 
-					if (board[bnumberx*y + x] != 0) {
+					if (board[bnumberx*y + x] != 0)
+                    {
 						SDL_SetRenderDrawColor(grender,
 								piece_color[board[bnumberx*y + x] - 1][0],
 								piece_color[board[bnumberx*y + x] - 1][1],
 								piece_color[board[bnumberx*y + x] - 1][2], 0);
 						SDL_RenderFillRect(grender, &box);
 						SDL_SetRenderDrawColor(grender, 50, 50, 50, 0);
-					} else {
+					}
+					else
+                    {
 						SDL_RenderDrawRect(grender, &box);
 					}
 				}
